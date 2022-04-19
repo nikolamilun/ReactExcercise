66b22b1c0f1da0ff1caebde299b926453f1b910d
Nikola Milun <nikola.milun2508@gmail.com>
2022-04-19 14:37:43 +0200

    Final changes
    Added comments, and published the repository to be public

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {useState} from "react";
 import './App.css';
 
 function App() {
+  // All the questions, right answer indexes and answer options
   const questions = [
     {
       question: 'Које године је био Косовски Бој?',
@@ -44,28 +45,33 @@ function App() {
       ]
     }
   ]
-
+  // Index of the current question
   const [currentIndex, setCurrentIndex] = useState(0);
+  // Number of correct answers
   const [corrects, setCorrects] = useState(0);
 
-
+  // Check if the answer is right 
   const handleAnswer = (answer) => {
     if(answer == questions[currentIndex].right)
         setCorrects(corrects + 1);
     setCurrentIndex(currentIndex + 1);
   }
-
+  // Main
   return (
     <div className="App">
+      {/* A container for all of the content */}
       <div className = "container">
         {
+          // Check if all the questions have been answered
           (currentIndex < questions.length)?
           (
             <>
+               {/* Question */}
               <div className = "questionDiv">
                 <p>Питање број {currentIndex + 1}</p>
                 <p>{questions[currentIndex].question}</p>
               </div>
+              {/* Answer options */}
               <div className = "answersDiv">
                 {
                   questions[currentIndex].answers.map((item, index) => (
@@ -76,6 +82,7 @@ function App() {
             </>
           ):
           (
+            // End of the quiz message
             <div className="end">
               <p className="endmsg">Крај квиза!</p>
               <p className="endmsg">Број тачних: {corrects}</p>
@@ -86,5 +93,5 @@ function App() {
     </div>
   );
 }
-
+// Export
 export default App;
